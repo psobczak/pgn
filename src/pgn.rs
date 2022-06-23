f7195f8cf7b80dde3da1f2231ffa448081249297
Paweł Sobczak <pawel.sobczak0@gmail.com>
2022-06-24 00:25:37 +0200

    Removed lifetime on Move

--- src/pgn.rs
+++ src/pgn.rs
@@ -1,6 +1,6 @@
 use std::{collections::VecDeque, fs::File, io::BufRead, io::BufReader, path::Path};
 
-use chrono::NaiveDate;
+use chrono::{NaiveDate, NaiveTime};
 use thiserror::Error;
 
 #[derive(Debug, Hash, PartialEq, Eq, Error)]
@@ -9,8 +9,8 @@ pub enum TagError {
     NoOpeningSquareBracket,
     #[error("tag must end with ']'")]
     NoClosingSquareBracket,
-    #[error("tag {0} is not supported")]
-    UnknownTag(String),
+    #[error("unknown tag {0}")]
+    UnknownTag(String, String),
 }
 
 #[derive(Debug, Hash, PartialEq, Eq)]
@@ -33,37 +33,36 @@ pub enum Tag {
     TimeControl(String),
     Opening(String),
     Termination(String),
+    EndTime(NaiveTime),
+    UTCTime(NaiveTime),
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub enum Player<'a> {
-    Black(&'a str),
-    White(&'a str),
+pub enum Move {
+    Black(String),
+    White(String),
 }
 
 #[derive(Debug)]
-pub struct Pgn<'a> {
-    tags: Vec<Tag>,
-    moves: VecDeque<Player<'a>>,
+pub struct Pgn {
+    tags: Vec<Result<Tag, TagError>>,
+    moves: VecDeque<Move>,
 }
 
-impl Pgn<'_> {
+impl Pgn {
     pub fn new<P>(path: P) -> std::io::Result<Self>
     where
         P: AsRef<Path>,
     {
         let file = File::open(path)?;
+        let lines: Vec<String> = BufReader::new(file).lines().flatten().collect();
         Ok(Self {
-            tags: BufReader::new(file)
-                .lines()
-                .flatten()
-                .flat_map(|s| Tag::try_from(s.as_ref()))
-                .collect(),
-            moves: VecDeque::default(),
+            tags: parse_tags(&lines),
+            moves: parse_moves(&lines),
         })
     }
 
-    pub fn tags(&self) -> &[Tag] {
+    pub fn tags(&self) -> &[Result<Tag, TagError>] {
         self.tags.as_ref()
     }
 }
@@ -83,8 +82,8 @@ impl TryFrom<&str> for Tag {
         let value = value.replace('[', "").replace(']', "");
 
         let data = value.split_once(' ').unwrap();
-        let data = (data.0, data.1.replace('\"', ""));
-        match data {
+
+        match (data.0, data.1.replace('\"', "")) {
             ("Event", event) => Ok(Tag::Event(event)),
             ("Site", site) => Ok(Tag::Site(site)),
             ("Date", date) => Ok(Tag::Date(
@@ -111,35 +110,63 @@ impl TryFrom<&str> for Tag {
             ("TimeControl", time_control) => Ok(Tag::TimeControl(time_control)),
             ("Opening", opening) => Ok(Tag::Opening(opening)),
             ("Termination", termination) => Ok(Tag::Termination(termination)),
-            _ => Err(TagError::UnknownTag(data.0.to_string())),
+            ("EndTime", end_time) => Ok(Tag::EndTime(
+                NaiveTime::parse_from_str(&end_time, "%H:%M:%S %Z").unwrap(),
+            )),
+            ("UTCTime", utc_time) => Ok(Tag::UTCTime(
+                NaiveTime::parse_from_str(&utc_time, "%H:%M:%S").unwrap(),
+            )),
+            (_, unknown_data) => Err(TagError::UnknownTag(data.0.to_string(), unknown_data)),
         }
     }
 }
 
-impl<'a> From<(&'a str, &'a str)> for Player<'a> {
-    fn from(value: (&str, &'a str)) -> Self {
+impl From<(&str, &str)> for Move {
+    fn from(value: (&str, &str)) -> Self {
         let value = (value.0.parse::<u16>().unwrap(), value.1);
         match (value.0 % 2 == 0, value.1) {
-            (true, chess_move) => Player::Black(chess_move),
-            (false, chess_move) => Player::White(chess_move),
+            (true, chess_move) => Move::Black(chess_move.to_string()),
+            (false, chess_move) => Move::White(chess_move.to_string()),
         }
     }
 }
 
+fn parse_tags(lines: &[String]) -> Vec<Result<Tag, TagError>> {
+    lines
+        .iter()
+        .filter(|line| line.starts_with('['))
+        .map(|line| Tag::try_from(line.as_ref()))
+        .collect()
+}
+
+fn parse_moves(line: &[String]) -> VecDeque<Move> {
+    let line = line
+        .iter()
+        .filter(|line| !line.starts_with('[') && !line.is_empty())
+        .map(|l| l.as_ref())
+        .collect::<Vec<&str>>()
+        .join(" ");
+
+    let moves = line.split(pat)
+
+    println!("{}", line);
+
+    VecDeque::new()
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn should_properly_assing_player_to_move() {
+    fn should_properly_assing_move_to_move() {
         let first_chess_move = ("1", "e4");
         let second_chess_move = ("2", "c6");
         let third_chess_move = ("3", "Nf3");
         let fourth_chess_move = ("4", "exd5");
 
-        assert_eq!(Player::from(first_chess_move), Player::White("e4"));
-        assert_eq!(Player::from(second_chess_move), Player::Black("c6"));
-        assert_eq!(Player::from(third_chess_move), Player::White("Nf3"));
-        assert_eq!(Player::from(fourth_chess_move), Player::Black("exd5"));
+        assert_eq!(Move::from(first_chess_move), Move::White("e4".to_string()));
+        assert_eq!(Move::from(second_chess_move), Move::Black("c6".to_string()));
+        assert_eq!(Move::from(third_chess_move), Move::White("Nf3".to_string()));
+        assert_eq!(Move::from(fourth_chess_move), Move::Black("exd5".to_string()));
     }
 }
